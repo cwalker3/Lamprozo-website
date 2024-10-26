c06523b58f3ffa8a88b57245b5934abfe7a92a12
Alex Strait <keylum88@yahoo.com>
2024-10-26 12:03:06 -0700

    Plugin Boilerplate

---
+++ plugins/firefly-collective/firefly.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+    Plugin Name: Firefly Collective
+    Description: Core website features.
+    Author: Alex Strait
+    */
+    require_once plugin_dir_path(__FILE__) . 'includes/firefly-functions.php';
---
+++ plugins/firefly-collective/includes/const.php
@@ -0,0 +1,6 @@
+<?php
+	
+	define('RANDOM_ID', uniqid());
+	define('PLUGIN_PATH_BACKEND', '/wp-content/plugins/firefly-collective/includes/apps/backend');
+	define('THEME_PATH_WEB', '/wp-content/themes/firefly-collective');
+	define('FULL_THEME_PATH', ABSPATH . 'wp-content/themes/firefly-collective');
