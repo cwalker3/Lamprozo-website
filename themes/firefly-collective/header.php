bfe9122fb659063ee95ba8e135106154af752828
Alex Strait <keylum88@yahoo.com>
2024-10-27 12:38:43 -0700

    Fixed nav when logged in
    Nav was being partially blocked by wpadminbar

--- themes/firefly-collective/header.php
+++ themes/firefly-collective/header.php
@@ -10,8 +10,8 @@
 <body <?php body_class(); ?>>
     <div id="backdrop"></div>
 
-    <header>
-        <div id="nav-bar"></div>
+    <header<? if( is_user_logged_in() ) {?> class="user-header"<? } ?>>
+        <div id="nav-bar"<? if( is_user_logged_in() ) {?> class="user-nav"<? } ?>></div>
         <div>
             <img id="hamburger" src="<?php echo esc_url($args['theme-path'] . '/images/hamburger.webp'); ?>" alt="<?php esc_attr_e('Menu', 'your-theme-textdomain'); ?>">
         </div>
