72ff6e2d28a4822228617676340c540af9cc8c72
Alex Strait <alex@fireflycreative.io>
2025-05-27 13:20:43 -0700

    Added Stripe/Composer to Plugin

---
+++ plugins/firefly-collective/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e26dadc74a4543395776eaaab529fe5f9eaabb4a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e26dadc74a4543395776eaaab529fe5f9eaabb4a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v17.2.1',
+            'version' => '17.2.1.0',
+            'reference' => 'a84ec46e82e2d1f1dbd04cf332317923ade82ba6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
