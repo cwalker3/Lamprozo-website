4a58fb2207471235e4040b6aa9216a1930eeec5f
Alex Strait <alex@fireflycreative.io>
2025-06-16 14:28:49 -0700

    Format firefly.php

--- plugins/firefly-collective/firefly.php
+++ plugins/firefly-collective/firefly.php
@@ -1,25 +1,26 @@
 <?php
-/*
-Plugin Name: Firefly Collective
-Description: Core website features.
-Author: Alex Strait
-*/
 
-// Stripe
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-    error_log( 'Composer autoload not found in Firefly Collective plugin.' );
-    return;
-}
+    /*
+    Plugin Name: Firefly Collective
+    Description: Core website features.
+    Author: Alex Strait
+    */
 
-require_once __DIR__ . '/vendor/autoload.php';
-\Stripe\Stripe::setApiKey( STRIPE_SECRET_KEY );
+    // Stripe
+    if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+        error_log( 'Composer autoload not found in Firefly Collective plugin.' );
+        return;
+    }
 
-require_once plugin_dir_path(__FILE__) . 'includes/firefly-functions.php';
+    require_once __DIR__ . '/vendor/autoload.php';
+    \Stripe\Stripe::setApiKey( STRIPE_SECRET_KEY );
 
-register_activation_hook(__FILE__, 'firefly_collective_create_tables');
+    require_once plugin_dir_path(__FILE__) . 'includes/firefly-functions.php';
 
-define('LIVE_DEV_ENDPOINT', 'https://fireflycollective.org/wp-json/firefly-collective/v1/update_project');
+    register_activation_hook(__FILE__, 'firefly_collective_create_tables');
 
-// Pricing system
-register_activation_hook(__FILE__, 'firefly_collective_pricing_init');
-register_deactivation_hook(__FILE__, 'drop_ffc_pricing_tables');
+    define('LIVE_DEV_ENDPOINT', 'https://fireflycollective.org/wp-json/firefly-collective/v1/update_project');
+
+    // Pricing system
+    register_activation_hook(__FILE__, 'firefly_collective_pricing_init');
+    register_deactivation_hook(__FILE__, 'drop_ffc_pricing_tables');
