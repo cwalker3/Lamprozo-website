cc310cb17af0affee311885b409e109b5749b261
Alex Strait <alex@fireflycreative.io>
2025-07-18 17:06:12 -0700

    Fix login error main.js

--- themes/firefly-collective/assets/js/main.js
+++ themes/firefly-collective/assets/js/main.js
@@ -23,19 +23,19 @@ function isValidPhoneNumber(phoneNumber) {
 
 document.addEventListener('DOMContentLoaded', function () {
     if ( isPWA || window.location.href.match(/(login|admin)\.php/) ) return;
-    logoNameEle.addEventListener('pointerup', ()=>{window.location="/"});
+    if (logoNameEle) logoNameEle.addEventListener('pointerup', ()=>{window.location="/"});
     handleContactSticky();
     function handleContactSticky() {
         if (page === 'contact' || page === 'request-an-appointment' || page === 'dashboard' || page === 'app') return;
         const contactSticky = document.querySelector('#contact-sticky');
-        contactSticky.addEventListener('pointerup', ()=>{window.location='/request-a-quote';});
+    if (contactSticky) contactSticky.addEventListener('pointerup', ()=>{window.location='/request-a-quote';});
         let isVisible = false;
         function toggleContactSticky() {
-            if (!isVisible && window.scrollY > 1000) {
+            if (!isVisible && window.scrollY > 1000 && contactSticky) {
                 contactSticky.style.display = 'block';
                 isVisible = true;
             }
-            if (window.scrollY + window.innerHeight > document.body.offsetHeight - 200) {
+            if (window.scrollY + window.innerHeight > document.body.offsetHeight - 200 && contactSticky) {
                 contactSticky.style.display = 'none';
                 isVisible = false;
             }
