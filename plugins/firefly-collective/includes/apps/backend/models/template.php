4b715e597101735880afc9adc65532fc3e3504d1
Alex Strait <alex@fireflycreative.io>
2025-08-11 14:51:22 -0700

    Fix bug for get active template

--- plugins/firefly-collective/includes/apps/backend/models/template.php
+++ plugins/firefly-collective/includes/apps/backend/models/template.php
@@ -4,5 +4,5 @@
 
     // Define template system constants
     define('FIREFLY_COLLECTIVE_DEFAULT_TEMPLATE', 'default');
-    define('FIREFLY_COLLECTIVE_TEMPLATE_OPTION', 'firefly_collective_active_template');
+    define('FIREFLY_COLLECTIVE_TEMPLATE_OPTION', 'firefly_collective_get_active_template');
     define('FIREFLY_COLLECTIVE_TEMPLATES_DIR', get_template_directory() . '/templates');
